22ccd4e6162b3f061b82fc4f2e6897b0ee450383
Md. Arifur Rahman <arifurroni@gmail.com>
2026-09-24 02:52:55 +0600

    my-book-vibe-app project file created

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import Banner from '@/components/homepage/Banner';
+import Books from '@/components/homepage/Books';
+import React from 'react';
+
+const page = () => {
+  return (
+    <div>
+      <Banner />
+      <Books />
+    </div>
+  );
+};
+
+export default page;
